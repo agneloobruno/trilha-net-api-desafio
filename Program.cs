0fffc2e25e0bd8a8097ca7e40691a55f3255cff0
Bruno Agnelo De Almeida <3714194@empresa.mti.mt.gov.br>
2025-10-17 17:05:42 -0400

    Altera configuração do banco de dados para MySQL e adiciona migrações iniciais

--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@ using TrilhaApiDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+var connectionString = builder.Configuration.GetConnectionString("ConexaoPadrao");
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 28)); // Ajuste a versão do seu MySQL se necessário
 
+builder.Services.AddDbContext<OrganizadorContext>(options =>
+    options.UseMySql(connectionString, serverVersion));
+    
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
